892b6ffd1e1a3882971d3bee867315e640343b03
chinakids <494063570@qq.com>
2016-01-09 18:28:15 +0800

    增加重启模块，完善注释

--- utils/reStart.js
+++ utils/reStart.js
@@ -4,29 +4,34 @@ import S from './../conf/setting';
 /**
  * [reStart 重启服务器]
  */
-function E(){
+
+//命令行执行方法
+function E(cmdStr){
+  logger('error','执行:'+cmdStr);
   childProcess.exec(cmdStr, function(err,stdout,stderr){
     if(err) {
-      logger('错误:'+stderr);
+      logger('error','错误:'+stderr);
     } else {
-      logger('成功');
+      logger('error','成功');
     }
   });
 };
 
+//重启函数
 function reStart(){
+  //重启pm2方法
   const rePm2 = () => {
 
   }
-
+  //重启node进程方法
   const reNode = () => {
 
   }
-
+  //重启supervisor方法
   const reSupervisor = () => {
 
   }
-
+  //重启forever方法
   const reForever = () => {
 
   }
